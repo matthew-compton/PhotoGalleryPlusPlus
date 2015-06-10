eba3b2f46636f37b412e9dcf4fe2406ffa2321a3
matthew-compton <compton@bignerdranch.com>
2015-06-10 12:17:13 -0400

    Added testing support

---
+++ app/src/test/java/com/ambergleam/android/photogallery/ApplicationTest.java
@@ -0,0 +1,14 @@
+package com.ambergleam.android.photogallery;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ApplicationTest {
+
+    @Test
+    public void testSetup() {
+        assertThat(true).isEqualTo(true);
+    }
+
+}
